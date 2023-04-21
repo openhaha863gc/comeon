6c80fac2b05180910a3c8da3bd5a1578b3f8b0ad
openhaha863gc <124115809+openhaha863gc@users.noreply.github.com>
2023-04-21 11:42:25 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
-TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
-SS_WSPATH=${SS_WSPATH:-'/shadowsocks'}
+UUID=${UUID:-'3192a2e2-fc54-451c-b50b-4db04ef18511'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/3192a2e2-fc54-451c-b50b-4db04ef18511-vmess'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/3192a2e2-fc54-451c-b50b-4db04ef18511-vless'}
+TROJAN_WSPATH=${TROJAN_WSPATH:-'/3192a2e2-fc54-451c-b50b-4db04ef18511-trojan'}
+SS_WSPATH=${SS_WSPATH:-'/3192a2e2-fc54-451c-b50b-4db04ef18511-shadowsocks'}
 
 generate_config() {
   cat > config.json << EOF
